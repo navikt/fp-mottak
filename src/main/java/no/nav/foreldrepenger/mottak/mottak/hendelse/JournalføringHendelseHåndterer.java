0ed8b7e231ad6c66048b5b415f708d1671a074a9
espenwaaga <espen.waaga@nav.no>
2025-12-04 10:53:21 +0100

    Revert "TFP-6438 (Fase 4): Fjerner ekstra delay ifm overgang fra fordel til mottak. (#15)"
    This reverts commit 218cedc5ddd1b7b55f3e6e620ee9ed90896ef8f2.

--- src/main/java/no/nav/foreldrepenger/mottak/mottak/hendelse/JournalføringHendelseHåndterer.java
+++ src/main/java/no/nav/foreldrepenger/mottak/mottak/hendelse/JournalføringHendelseHåndterer.java
@@ -136,8 +136,15 @@ public class JournalføringHendelseHåndterer implements KafkaMessageHandler<Str
             melding.setEksternReferanseId(eksternReferanse);
         }
         var oppdatertTaskdata = melding.getProsessTaskData();
-        oppdatertTaskdata.setNesteKjøringEtter(LocalDateTime.now().plus(delay));
+        oppdatertTaskdata.setNesteKjøringEtter(LocalDateTime.now().plus(delay).plus(deploymentDelay()));
         taskTjeneste.lagre(oppdatertTaskdata);
+    }
+
+    private static Duration deploymentDelay() {
+        if (ENV.isLocal()) {
+            return Duration.ZERO;
+        }
+        return Duration.ofMinutes(10);
     }
 
     @Override
