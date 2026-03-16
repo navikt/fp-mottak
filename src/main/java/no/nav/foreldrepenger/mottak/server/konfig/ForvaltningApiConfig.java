88be77c9525e69c02a28418164f2ebd267368ea3
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-03-16 18:54:14 +0100

    TFP-6623 bruke rest-oppsett og openapi fra felles (#59)

--- src/main/java/no/nav/foreldrepenger/mottak/server/konfig/ForvaltningApiConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/server/konfig/ForvaltningApiConfig.java
@@ -1,7 +1,6 @@
 package no.nav.foreldrepenger.mottak.server.konfig;
 
 import static no.nav.foreldrepenger.mottak.server.konfig.ApiConfig.getApplicationProperties;
-import static no.nav.foreldrepenger.mottak.server.konfig.ApiConfig.getFellesConfigClasses;
 
 import java.util.Set;
 
@@ -9,34 +8,34 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import jakarta.ws.rs.ApplicationPath;
+import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.mottak.server.forvaltning.ForvaltningRestTjeneste;
-import no.nav.foreldrepenger.mottak.server.konfig.swagger.OpenApiUtils;
-import no.nav.foreldrepenger.mottak.server.sikkerhet.ForvaltningAuthorizationFilter;
 import no.nav.vedtak.felles.prosesstask.rest.ProsessTaskRestTjeneste;
+import no.nav.vedtak.openapi.OpenApiUtils;
+import no.nav.vedtak.server.rest.ForvaltningAuthorizationFilter;
+import no.nav.vedtak.server.rest.FpRestJackson2Feature;
 
 @ApplicationPath(ForvaltningApiConfig.API_URI)
 public class ForvaltningApiConfig extends ResourceConfig {
     public static final String API_URI = "/forvaltning/api";
 
+    private static final Environment ENV = Environment.current();
+
     public ForvaltningApiConfig() {
+        register(FpRestJackson2Feature.class);
         register(ForvaltningAuthorizationFilter.class); // Autorisering – drift
-        registerClasses(getFellesConfigClasses());
         registerOpenApi();
         registerClasses(getForvaltningKlasser());
         setProperties(getApplicationProperties());
     }
 
     public static Set<Class<?>> getForvaltningKlasser() {
-        return Set.of(
-            ProsessTaskRestTjeneste.class,
-            ForvaltningRestTjeneste.class
-        );
+        return Set.of(ProsessTaskRestTjeneste.class, ForvaltningRestTjeneste.class);
     }
 
     private void registerOpenApi() {
-        OpenApiUtils.openApiConfigFor("Fpmottak - mottak av dokumenter via kafka og journalføringsoppgaver", this)
-            .registerClasses(getForvaltningKlasser())
-            .buildOpenApiContext();
+        OpenApiUtils.setupOpenApi("Fpmottak - mottak av dokumenter via kafka og journalføringsoppgaver",
+            ENV.getProperty("context.path", "/fpmottak"), getForvaltningKlasser(), this);
         register(OpenApiResource.class);
     }
 }
